05bd76e5aedb6292c82587b66da0e60466bf50fb
Miten Kashiyani <miten.kashiyani96@gmail.com>
2020-05-23 16:26:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,15 @@
 function happyHolidays() {
   return `Happy Holidays!`
 }
+
+function happyHolidaysTo(name) {
+  return `Happy Holidays, ${name}!`
+}
+
+function happyCustomHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday, days) {
+  return `It's ${days} until ${holiday}!`
+}
